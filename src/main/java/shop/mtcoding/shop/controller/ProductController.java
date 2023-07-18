3eadb21deacd0ffa3e9606d8c9e98e4f1bc4c7f8
wlstjdgns <jin009471@naver.com>
2023-07-18 16:37:32 +0900

    스프링부트 쇼핑몰만들기

---
+++ src/main/java/shop/mtcoding/shop/controller/ProductController.java
@@ -0,0 +1,16 @@
+package shop.mtcoding.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+    @GetMapping("/")
+    public String home(){
+        return  "home";
+    }
+    @GetMapping("write")
+    public String write(){
+        return  "write";
+    }
+}
---
+++ src/test/java/shop/mtcoding/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
